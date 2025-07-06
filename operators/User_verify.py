bbc3599c32028c3ed8b6bd8c134ef0e19b1cf569
Bhavesh Salvi <162792561+bhaves19h@users.noreply.github.com>
2025-07-06 14:26:29 +0530

    Create User_verify.py

---
+++ operators/User_verify.py
@@ -0,0 +1,5 @@
+#it will written true if both condition are true
+
+user = input("Enter username: ")
+paswd = input("Enter password: ")
+print("Login Successful" ,user == "Admin" , paswd == "*****") 
